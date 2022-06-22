854347be7b7b326afc1e3c651f09c9dcc59b7250
JeffAllison <itsjallison@gmail.com>
2022-06-22 11:08:03 -0700

    addedd filter array function

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ forEach(assets, (obj) => (obj.value *= 2)); // no return value
 // [{ value: 6 }, { value: 8 }, { value: 12 }]
 console.log(assets);
 
-function map(arr, callback) { 
+function map(arr, callback) {
   let newArray = [];
-  for (let i = 0; i< arr.length; i++){
-    const item = arr [i];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
     const modifiedItem = callback(item);
     newArray[newArray.length] = modifiedItem;
   }
@@ -22,3 +22,18 @@ function map(arr, callback) {
 const myArray = [1, 6, 5];
 const newArray = map(myArray, (num) => num ** 2);
 console.log(newArray);
+
+function filter(arr, callback) {
+  const filteredArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+
+    if (callback(item)) {
+      filteredArr[filteredArr.length] = item;
+    }
+  }
+  return filteredArr;
+}
+
+console.log(filter([2, 6, 5], (n) => (n % 2 === 0) | [(2, 6)]));
