b2cfcd9f2dc6852ad5813ad872903c445d677d01
Khayman King <khaymanak@gmail.com>
2022-06-22 10:16:49 -0700

    attmepting map function in index.js

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ forEach(assets, (obj) => (obj.value *= 2)); // no return value
 // assets is now:
 // [{ value: 6 }, { value: 8 }, { value: 12 }]
 console.log(assets);
+
+function map(arr, callback) { 
+  let newArray = [];
+  arr[i] = arr[i + 1];
+  callback(newArray);
+  return newArray;
+}
+const myArray = [1, 2, 3, 4];
+const newArray = map(myArray, (num) => num * 2);
+console.log(newArray);
